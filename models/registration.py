856a31375f0f3d4576e4974734ba5e2883e4febb
ebm5630 <ebm5630@psu.edu>
2025-11-30 16:28:34 -0500

    finished nmodel/registration.py

--- models/registration.py
+++ models/registration.py
@@ -43,7 +43,6 @@ class Registration:
                         buyin_amount,
                         registered_status,
                     ),
-                    
                 )
                 r_id = db.cursor.lastrowid
             return Registration(
@@ -57,11 +56,78 @@ class Registration:
             print(f"Database Error while creating Registration entry: {e}")
             return False
 
-        
     @staticmethod
     def get_by_id(r_id):
         "All players who are registered"
         sql = """
-        SELECT player_id 
+        SELECT player_id FROM Registration WHERE registeration_id=%s 
         """
-        pass 
+        try:
+            with Database() as db:
+                row = db.fetchone(sql, (r_id,))
+            if row:
+                return Registration(**row)
+            return None
+        except Error as e:
+            print(f"Database error while getting Registered by ID: {e}")
+            return None
+
+    @staticmethod
+    def get_all():
+        sql = "SELECT * FROM Registration"
+        try:
+            with Database() as db:
+                rows = db.fetchall(sql)
+            return [Registration(**row) for row in rows]
+        except Error as e:
+            print(f"Database error while getting all Registration: {e}")
+            return []
+
+    def update(self):
+        sql = """
+        UPDATE Registration 
+        SET registered_player_id = %s, registered_tournament_id = %s, buyin_amount = %s, registered_status = %s 
+        WHERE registration_id = %s
+        """
+        try:
+            with Database() as db:
+                db.execute(
+                    sql,
+                    (
+                        self.registered_player_id,
+                        self.registered_tournament_id,
+                        self.buyin_amount,
+                        self.registered_status,
+                        self.registration_id,
+                    ),
+                )
+            return True
+        except Error as e:
+            print(f"Database error while updating registration: {e}")
+            return False
+
+    def delete(self):
+        sql = "DELETE FROM Registration WHERE registration_id = %s"
+        try:
+            with Database() as db:
+                db.execute(sql, (self.registration_id,))
+                return True
+        except Error as e:
+            print(f"Database error while deleting registration: {e}")
+            return False
+
+    def delete_player(self):
+        sql = """
+        DELETE FROM Registration
+        WHERE registered_player_id = %s 
+        AND registered_tournament_id = %s
+        """
+        try:
+            with Database() as db:
+                db.execute(
+                    sql, (self.registered_player_id, self.registered_tournament_id)
+                )
+                return True
+        except Error as e:
+            print(f"Databse error while deleting registered player")
+            return False
